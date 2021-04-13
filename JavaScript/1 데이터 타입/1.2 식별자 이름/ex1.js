9d4577e5612567957c5f1a134528c496a6819fd9
cg2z <97cg2z@gmail.com>
2021-04-14 07:11:14 +0900

    identifier

---
+++ JavaScript/1 데이터 타입/1.2 식별자 이름/ex1.js
@@ -0,0 +1,15 @@
+// Good
+let identifier = "a";
+let identifier10 = "b";
+let _identifier = "c";
+let $identifier = "d";
+let Identifier = "e";
+let IDENTIFIER = "f";
+let identifierπ = "g";
+
+// Bad
+//let break
+//let 123identifier
+//let has space 
+
+console.log(identifier + identifier10 + _identifier + $identifier + Identifier + IDENTIFIER + identifierπ)
