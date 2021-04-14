89e045a1a132444617747ca2b2a879c9523c9e19
czvy <congkvzcy@gmail.com>
2021-04-14 18:50:32 +0900

    literal

---
+++ JavaScript/1 데이터 타입/1.3 리터럴/ex1.js
@@ -0,0 +1,11 @@
+let room1 = "suite_room"
+// "suite_room"(따옴표 안)은 리터럴 이다.
+
+let currentRoom = room1;
+// 이제 currentRoom의 값은 room1의 값(suite_room)과 같다.
+
+// currentRoom = suite_room
+// ERROR - suite_room이라는 식별자가 없다.
+
+console.log(room1);
+console.log(currentRoom);
