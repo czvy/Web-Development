31fc1bdb36450adbfc31e989dd746a7b7521f948
czvy <congkvzcy@gmail.com>
2021-04-27 07:07:20 +0900

    Number

---
+++ JavaScript/1 데이터 타입/1.5 숫자/ex1.js
@@ -0,0 +1,21 @@
+let count = 10; // 숫자 리터럴, count는 더블이다.
+const blue = 0x0000ff; // 16진수. 16진수 ff는 10진수 255와 같다.
+const umask = 0o0022 // 8진수. 8진수 22는 십진수 18과 같다.
+const binary = 0b1111; //2진수 2진수 1111은 십진수 15와 같다. 
+const roomTemp = 21.5; //십진수
+const c = 3.0e6; // 지수 (3.0 x 10^6 = 3,000,000)
+const e = -1.6e-19; // 지수 (-1.6 x 10^-19 = 0.00000000000000000016)
+const inf = Infinity;
+const nInf = -Infinity;
+const nan = NaN; // 숫자가 아님
+
+console.log(count);
+console.log(blue);
+console.log(umask);
+console.log(binary);
+console.log(roomTemp);
+console.log(c);
+console.log(e);
+console.log(inf);
+console.log(nInf);
+console.log(nan);
